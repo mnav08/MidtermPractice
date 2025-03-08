c47f10d1fec7fc5bc4e452205dbd9e95e1bdc9c0
mnav08 <navas.20moi@gmail.com>
2025-03-08 15:28:47 -0600

    for loop Signed-off-by:mnav08 <navas.20moi@gmail.com>

---
+++ excersices_/src/excersices_/Excersices_.java
@@ -0,0 +1,26 @@
+package excersices_;
+import javax.swing.JOptionPane;
+
+public class Excersices_ {
+    public static void main(String[] args) {
+        //sum of even numbers
+        /*
+        int num;
+        int sum = 0;
+        String input = JOptionPane.showInputDialog("Enter a positive integer:");
+        // Convert input string to integer
+        num = Integer.parseInt(input);
+      
+        for(int i=1; i<=num;i++){
+            if(i%2==0){
+                sum+=i;
+            }
+        } JOptionPane.showMessageDialog(null, "The sum of even numbers from 1 to " + num + " is: " + sum);
+*/
+        
+        
+       
+        
+    }
+    
+}
---
+++ excersices_/src/excersices_/Objeto.java
@@ -0,0 +1,5 @@
+package excersices_;
+public class Objeto {
+    
+    
+}
